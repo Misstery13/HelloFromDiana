0106d06b67162c527074a2649446b2a75b12e449
decreo17 <decreogithub@gmail.com>
2022-01-26 06:36:52 +0800

    Added my greetings and fix class name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Diana");
+        System.out.println("Hello Diana, my name is Dhener.");
     }
 }
