b85659a1909f2fd64304e05ac9a53861e50f8f0c
Laura Nuñez <77087152+Ichlaura@users.noreply.github.com>
2022-01-26 12:26:28 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Diana");
         System.out.println("Hello Diana, my name is Dhener.");
+        System.out.println("Hello Diana, my name is Laura.");
     }
 }
